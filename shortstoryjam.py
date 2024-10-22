ca68ebd4bcd034049d6e02fa2db6f71e21dbe97d
Anna L. Sheaffer <asheaf01@eecs.tufts.edu>
2024-10-22 16:18:45 -0400

    Cleaning text code

--- shortstoryjam.py
+++ shortstoryjam.py
@@ -1,12 +1,23 @@
 import requests
 import re
 import string
+import os
 stopwords_list = requests.get("https://gist.githubusercontent.com/rg089/35e00abf8941d72d419224cfd5b5925d/raw/12d899b70156fd0041fa9778d657330b024b959c/stopwords.txt").content
 stopwords = list(set(stopwords_list.decode().splitlines()))
 
+# poe's story data
+poe_src = "data/poe-stories"
+
+content = ''
+for filename in os.listdir(poe_src):
+    file_path = os.path.join(poe_src, filename)
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as file:
+            content += " " + file.read()
 
 def remove_stopwords(words):
-    list_ = re.sub(r"[^a-zA-Z0-9]", " ", words.lower()).split())
+    list_ = re.sub(r"[^a-zA-Z0-9]", " ", words.lower()).split()
     return [itm for itm in list_ if itm not in stopwords]
 
 
@@ -17,4 +28,9 @@ def clean_text(text):
     text = re.sub('[\d\n]', ' ', text)
     return ' '.join(remove_stopwords(text))
 
+no_stopwords = remove_stopwords(content)
 
+
+content = clean_text(content)
+
+print(content)
